6b179b46e7a6a2da2c84c6c72b4498fc60aea445
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-17 14:34:10 +0200

    feat: Add contructor and toString mehod with City to Parent Class.

--- POOEx3Heritage/src/fr/fms/entities/Capital.java
+++ POOEx3Heritage/src/fr/fms/entities/Capital.java
@@ -6,6 +6,23 @@ package fr.fms.entities;
 /**
  * 
  */
-public class Capital {
+public class Capital extends City {
+	private String monument;
 
+	/**
+	 * @param name
+	 * @param country
+	 * @param nbInhabitants
+	 * @param monument
+	 */
+	public Capital(String name, String country, int nbInhabitants, String monument) {
+		super(name, country, nbInhabitants);
+		this.monument = monument;
+	}
+
+	@Override
+	public String toString() {
+		return super.getName() + " , " + super.getCountry() + " , " + super.getNbInhabitants()
+				+ " d'habitants monument : " + this.monument;
+	}
 }
