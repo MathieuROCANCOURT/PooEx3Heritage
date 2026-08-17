0575a2d8d19f2660b744a740c1f792c11ee5975f
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-17 15:26:33 +0200

    feat: Add constructor.

--- POOEx3Heritage/src/fr/fms/entities/Employee.java
+++ POOEx3Heritage/src/fr/fms/entities/Employee.java
@@ -7,5 +7,36 @@ package fr.fms.entities;
  * 
  */
 public class Employee extends Person{
+	private String nameCompany;
+	private double salary;
+	
+	/**
+	 * 
+	 * @param lastName
+	 * @param firstName
+	 * @param salary
+	 * @param nameCompany
+	 */
+	public Employee(String lastName, String firstName, double salary, String nameCompany) {
+		super(lastName, firstName);
+		this.salary = salary;
+		this.nameCompany = nameCompany;
+	}
 
+	/**
+	 * 
+	 * @param lastName
+	 * @param firstName
+	 * @param old
+	 * @param address
+	 * @param bornCity
+	 * @param salary
+	 * @param nameCompany
+	 */
+	public Employee(String lastName, String firstName, int old, String address, City bornCity, double salary, String nameCompany) {
+		super(lastName, firstName, old, address, bornCity);
+		this.salary = salary;
+		this.nameCompany = nameCompany;
+	}
+	
 }
