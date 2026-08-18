f35c5b6acbde321b8745ff291f6510dc32d7e066
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-18 08:59:39 +0200

    test: Add test Salesman class.

---
+++ POOEx3Heritage/src/fr/fms/entities/Test.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package fr.fms.entities;
+
+/**
+ * 
+ */
+public class Test {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/** Test Capital, City and Person class **/
+		Employee gate = new Employee("Gate", "Bill", 65, "USA", new Capital("Washington", "USA", 7_600_000, "W-M"), 100_000.0,
+				"Fondation B&M");
+		Employee musk = new Employee("Musk", "Elon", 49, "Palo Alto", new City("Pretoria", "Afrique du Sud", 800_000),
+				150_000.0, "Tesla");
+
+		System.out.println(gate);
+		System.out.println(musk);
+		
+		/** Test Salesman class **/
+		Salesman dupont = new Salesman("Dupont", "Robert", 50, "Rue des rosiers à Toulouse", new City("Limoges", "France"), "brico", 5.0);
+		
+		System.out.println(dupont);
+	}
+
+}
