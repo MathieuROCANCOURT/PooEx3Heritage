761d172861a1c7715d792ed432988528363a3af5
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-18 08:50:59 +0200

    feat: Add a constructor.

--- POOEx3Heritage/src/fr/fms/entities/Salesman.java
+++ POOEx3Heritage/src/fr/fms/entities/Salesman.java
@@ -7,5 +7,23 @@ package fr.fms.entities;
  * 
  */
 public class Salesman extends Person {
+	private String nameCompany;
+	private double revenu;
 
+	/**
+	 * @param lastName
+	 * @param firstName
+	 * @param old
+	 * @param address
+	 * @param bornCity
+	 * @param nameCompany
+	 * @param revenu
+	 */
+	public Salesman(String lastName, String firstName, int old, String address, City bornCity, String nameCompany, double revenu) {
+		super(lastName, firstName, old, address, bornCity);
+		this.nameCompany = nameCompany;
+		this.revenu = revenu;
+	}
+	
+	
 }
