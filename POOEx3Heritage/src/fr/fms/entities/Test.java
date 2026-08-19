e4792ada11b3b1b6668417a2ae2682d3e3ccb3a7
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-19 11:31:07 +0200

    chore: Add Test constructor and format file.

--- POOEx3Heritage/src/fr/fms/entities/Test.java
+++ POOEx3Heritage/src/fr/fms/entities/Test.java
@@ -9,41 +9,46 @@ package fr.fms.entities;
  * @author RocancourtM
  */
 public class Test {
+	public Test() {
+		super();
+	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		/** Test Capital, City and Person class **/
-		Employee gate = new Employee("Gate", "Bill", 65, "USA", new Capital("Washington", "USA", 7_600_000, "W-M"), 100_000.0,
-				"Fondation B&M");
+		Employee gate = new Employee("Gate", "Bill", 65, "USA", new Capital("Washington", "USA", 7_600_000, "W-M"),
+				100_000.0, "Fondation B&M");
 		Employee musk = new Employee("Musk", "Elon", 49, "Palo Alto", new City("Pretoria", "Afrique du Sud", 800_000),
 				150_000.0, "Tesla");
 
 		System.out.println(gate);
 		System.out.println(musk);
-		
+
 		/** Test Salesman class **/
-		Salesman dupont = new Salesman("Dupont", "Robert", 50, "Rue des rosiers à Toulouse", new City("Limoges", "France"), "brico", 5.0);
-		
+		Salesman dupont = new Salesman("Dupont", "Robert", 50, "Rue des rosiers à Toulouse",
+				new City("Limoges", "France"), "brico", 5.0);
+
 		System.out.println(dupont);
-		
+
 		/** Test error values **/
 		dupont.setRevenu(-5);
 		dupont.setRevenu(3.5);
-		
+
 		System.out.println(dupont);
-		
+
 		/** Create Employee and Salesman list **/
-		Person[] listPerson = new Person[] {gate, musk, dupont};
+		Person[] listPerson = new Person[] { gate, musk, dupont };
 
 		System.out.println("----------------------------------------------");
-		for (Person person: listPerson) {
+		for (Person person : listPerson) {
 			System.out.println(person);
 		}
 		System.out.println("----------------------------------------------");
-		
+
 		/** Display monthly salary of the Employee and Salesman in the list **/
-		for (Person person: listPerson) {
+		for (Person person : listPerson) {
 			person.displayMonthlySalary();
 		}
 	}
