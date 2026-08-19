ea9807da8f30e044afddd6789862089d14449f93
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-08-19 11:55:16 +0200

    docs: Add docstring to the Person methods.

--- POOEx3Heritage/src/fr/fms/entities/Person.java
+++ POOEx3Heritage/src/fr/fms/entities/Person.java
@@ -38,7 +38,7 @@ public abstract class Person {
 	 * @param firstName First name.
 	 * @param old       Age.
 	 * @param address   Address of the person.
-	 * @param capital	Capital of birth.
+	 * @param capital   Capital of birth.
 	 */
 	protected Person(String lastName, String firstName, int old, String address, Capital capital) {
 		this.lastName = lastName;
@@ -49,21 +49,34 @@ public abstract class Person {
 	}
 
 	/**
-	 * @return
+	 * Check if the character is born in France.
+	 * 
+	 * @return true if born city is France, else false.
 	 */
 	public boolean bornFrance() {
 		return this.bornCity.getCountry().contains("France");
 	}
 
 	/**
-	 * @return
+	 * Check if the address of the character is in Paris.
+	 * 
+	 * @return true if address contains Paris, else false.
 	 */
 	public boolean inParis() {
 		return this.address.contains("Paris");
 	}
 
+	/**
+	 * Calculate the monthly salary of the person.
+	 * 
+	 * @return Monthly salary
+	 */
 	public abstract double monthlySalary();
 
+	/**
+	 * Display monthly salary with 2 digits after the comma with the first name and
+	 * the last name.
+	 */
 	public void displayMonthlySalary() {
 		System.out.print("Salaire mensuel de " + this.firstName + " " + this.lastName + " : ");
 		System.out.printf("%.2f", this.monthlySalary());
